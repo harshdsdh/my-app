e0e1ce63336a0240c37dc3ad6b2c834a86350497
harshdsdh <harsh.dsdh@gmail.com>
2019-07-25 00:37:23 -0400

    Updates

---
+++ precache-manifest.02ac2d494eb44709c66ddaeacaec3c2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f7143d1d8ccf3207621d80c94306b7c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "9726e981984061e91e42",
+    "url": "/my-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9a92db59f189398a422f",
+    "url": "/my-app/static/css/main.2dc5f43e.chunk.css"
+  },
+  {
+    "revision": "9726e981984061e91e42",
+    "url": "/my-app/static/js/2.09a23faa.chunk.js"
+  },
+  {
+    "revision": "9a92db59f189398a422f",
+    "url": "/my-app/static/js/main.6e22c591.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
